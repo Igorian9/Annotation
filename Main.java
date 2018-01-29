5bcaecac535d797f84f7381d10be675dbe9b96ff
Igor <igor_igorovuich@i.ua>
2018-01-29 18:48:25 +0200

    second commit

--- Main.java
+++ Main.java
@@ -4,17 +4,12 @@ import java.lang.reflect.Method;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start");
         final Class<?> myClass = Something.class;
         Method[] methods = myClass.getDeclaredMethods();
         for (Method method : methods) {
             if (method.isAnnotationPresent(MyAnnotation.class)) {
-                try {
                     MyAnnotation ma = method.getAnnotation(MyAnnotation.class);
                     System.out.println(ma.a() + " " + ma.b());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
             }
 
         }
